1fe89f019ec59fe515c5162e2be60db2623e4c00
Gregory2786 <114622096+Gregory2786@users.noreply.github.com>
2025-02-07 20:31:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -176,7 +176,6 @@ startGameButton.addEventListener("click", () => {
 
         // Oculta a área de entrada
         document.querySelector(".name-input").style.display = "none";
-
         snake = [initialPosition]; // Reinicia a cobrinha
         direction = undefined; // Reseta a direção
         gameLoop(); // Inicia o loop do jogo
